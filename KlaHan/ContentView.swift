33e80a7d1636afa48937d24f9c5a4cf3d2701dbe
Rawin14 <rawinz14@gmail.com>
2025-04-26 17:27:42 +0700

    Initial Commit

---
+++ KlaHan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KlaHan
+//
+//  Created by COSCI_LAB on 26/4/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KlaHan/KlaHanApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlaHanApp.swift
+//  KlaHan
+//
+//  Created by COSCI_LAB on 26/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct KlaHanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
